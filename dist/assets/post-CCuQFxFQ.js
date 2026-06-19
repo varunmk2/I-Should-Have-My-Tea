17f021f1792ab581172df53b34c104f9ac24b790
Varun <varunkolambekar@gmail.com>
2026-06-19 15:36:34 +0530

    Fix blog useMemo reference error and clean dependencies

---
+++ dist/assets/post-CCuQFxFQ.js
@@ -0,0 +1,15 @@
+const n=`---\r
+title: The Daily Obstacle Course\r
+slug: daily-obstacle\r
+date: June 19, 2026\r
+excerpt: Exploring the daily adjustments we make in our design workflows.\r
+featuredImage: null\r
+---\r
+\r
+# My First Markdown Post\r
+\r
+Writing code inside standard array strings stinks! Now we can write natively using standard **Markdown formatting**. \r
+\r
+### Code Example:\r
+\`\`\`javascript\r
+const greet = () => console.log("Hello, world!");`;export{n as default};
