d10c0f7cac30acc35aa2896f91d7258d3b38f6b3
Varun <varunkolambekar@gmail.com>
2026-06-19 16:03:36 +0530

    Configure global route tracking with BrowserRouter basename

--- quiet-blog/src/main.jsx
+++ quiet-blog/src/main.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { BrowserRouter } from 'react-router-dom' // Added this import
 import './styles/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {/* Wrap your App and configure basename for GitHub Pages */}
+    <BrowserRouter basename="/I-Should-Have-My-Tea">
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
 )
